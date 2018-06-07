46ec9be0e5a8977b192af3546e5d521e407720ea
jrandiny <jrandiny2@gmail.com>
2018-06-07 16:30:31 +0700

    Add login and logout functionality

--- js/user.js
+++ js/user.js
@@ -19,32 +19,50 @@ function logout(){
 function login(){
   console.log($("#emailLogin").val());
   console.log($("#passwordLogin").val());
-  Cookies.set("token","xxx");
+  console.log("do");
+  $.ajax({
+    method: "POST",
+    url: "https://tec-rest.didithilmy.com/public/index.php/login",
+    data: { email: $("#emailLogin").val(), password: $("#passwordLogin").val() }
+  })
+  .done(function( msg ) {
+    console.log("done E= "+msg.message);
+    if(typeof msg.error !== "undefined"){
+      console.log("Error");
+    }else {
+      Cookies.set("token",msg.token);
+      console.log(Cookies.get("token"));
+      location.reload();
+    }
+  });
   Cookies.set("name","testing");
+
+}
+
+function reloadNavElement(){
+  if(isLoggedIn()){
+    $("#loginMenuNav").text(Cookies.get("name"));
+    $("#loginMenuDrop").remove();
+
+    $('#logoutButton').click(logout);
+
+
+  }else{
+    $("#userMenuDrop").remove();
+  }
 }
 
 $( document ).ready(function() {
-    if(isLoggedIn()){
-      $("#loginMenuNav").text(Cookies.get("name"));
-      $("#loginMenuDrop").remove();
-
-      $('#logoutButton').click(logout);
-
-
-    }else{
-      $("#userMenuDrop").remove();
-      $("#signinButton").click(login);
-
-    }
+  reloadNavElement();
 });
 
 function testAJAX(){
   $.ajax({
-    method: "GET",
-    url: "https://tec-rest.didithilmy.com/public/index.php/api/login",
+    method: "POST",
+    url: "https://tec-rest.didithilmy.com/public/index.php/login",
     data: { email: "demokader@tec.itb.ac.id", password: "passwordbaruterkini" }
   })
   .done(function( msg ) {
-    alert( "Data Saved: " + msg );
+    alert( "Data Saved: " + msg.token );
   });
 }
