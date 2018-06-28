c1fbd245f63efefe3343f6cd06d5d8a4d0f7809d
jrandiny <jrandiny2@gmail.com>
2018-06-28 17:31:12 +0700

    Fix typo

--- js/profile.js
+++ js/profile.js
@@ -80,7 +80,7 @@ function getUserData(userId){
                     </tr>
                     <tr class="d-flex">
                       <td class="col-3">Panggilan</td>
-                      <td class="col-9">: `+ profileData.nick +`</td>
+                      <td class="col-9">: `+ profileData.nickname +`</td>
                     </tr>
                     <tr class="pb-3 d-flex">
                       <td class="col-3">Lunas</td>
